63fe743130871d6f0f73b808ab265e080f6b1ffc
marta <m.nowak033@gmail.com>
2026-09-10 19:27:37 +0200

    Install dotenv to get access to env variables

--- db.php
+++ db.php
@@ -1,9 +1,14 @@
 <?php
 
+require __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();   
+
 $host = 'localhost';
 $database = 'crud_php';
 
-$conn = new mysqli($host, $_ENV["DB_USER"], $database, $_ENV["DB_PASSWORD"]);
+$conn = new mysqli($host, $_ENV["DB_USER"], $_ENV["DB_PASSWORD"], $database);
 
 if ($conn->connect_error)
     {
